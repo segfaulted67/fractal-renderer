b4f2950f94d2b8e1258243e18f5c893cc3005e74
larry <segfaulted67@gmail.com>
2025-12-15 16:45:47 +0530

    Added screenshot

--- fractal-renderer.c
+++ fractal-renderer.c
@@ -3,12 +3,25 @@
 #include <complex.h>
 #include <raylib.h>
 
-#define WIDTH     1500
-#define HEIGHT    1000
+#define WIDTH  1500
+#define HEIGHT 1000
 
 float complex mandelbrot(float complex z, float complex c)
 {
   return z * z + c;
+}
+
+float complex burning_ship(float complex z, float complex c)
+{
+    float zr = fabsf(crealf(z));
+    float zi = fabsf(cimagf(z));
+    float complex w = zr + I * zi;
+    return w * w + c;
+}
+
+float complex mandelbar(float complex z, float complex c)
+{
+    return conjf(z) * conjf(z) + c;
 }
 
 float is_in_fractal(float complex (* zn)(float complex, float complex), float complex c, int max_iter, float escape_rad)
@@ -16,7 +29,7 @@ float is_in_fractal(float complex (* zn)(float complex, float complex), float co
   float complex z = 0.0f + I * 0.0f;
   for (int i = 0; i < max_iter; i++) {
     z = zn(z, c);
-    if (cabsf(z) > escape_rad)
+    if (crealf(z) * crealf(z) + cimagf(z) * cimagf(z) > escape_rad * escape_rad)
       return i;
   }
   return 0;
@@ -25,27 +38,29 @@ float is_in_fractal(float complex (* zn)(float complex, float complex), float co
 int main()
 {
   int max_iter = 250;
-  float escape_rad = 10.0f;
+  float escape_rad = 2.0f;
   InitWindow(WIDTH, HEIGHT, "mandelbrot set");
   SetTargetFPS(60);
 
   while(!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground((Color) { 0.0f, 0.0f, 0.0f, 1.0f });
+    float x = 0.0f, y = 0.0f;
     for (int px = 0; px < WIDTH; px++) {
       for (int py = 0; py < HEIGHT; py++) {
-        float x = -2.0 + 4.0 * px / WIDTH;
-        float y = -2.0 + 4.0 * py / HEIGHT;
+        x = -2.5 + 4.0 * px / WIDTH;
+        y = -2.0 + 4.0 * py / HEIGHT;
 
         float complex c = x + I * y;
         int iter = is_in_fractal(mandelbrot, c, max_iter, escape_rad);
+
 
         Color color;
         if (iter == 0)
           color = BLACK;
         else {
-          float r = (5 * iter) % 255;
-          float g = (5 * iter) % 255;
+          float r = (2 * iter) % 255;
+          float g = (2 * iter) % 255;
           float b = (10 * iter) % 255;
           color = (Color) { r, g, b, 255 };
         }
